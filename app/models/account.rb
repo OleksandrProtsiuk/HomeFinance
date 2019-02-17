a2fc67335fefdb0750a34737be998473e4f4ea94
alex <alex_is_ok@yahoo.com>
2019-02-17 23:21:25 +0200

    account model validates added

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,8 @@
 class Account < ApplicationRecord
+
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: 2..120
+  validates :value, presence: true
+  validates :status, presence: true, inclusion: %w(income costs saved)
+  validates :category_id, presence: true
+
 end
