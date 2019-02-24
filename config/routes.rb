4aba8ebe544d339afa6e36ed6e079b2bb9c2ca0a
alex <alex_is_ok@yahoo.com>
2019-02-24 17:32:21 +0200

    routes edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#index', as: 'root'
+
   get 'static_pages/index'
   get 'static_pages/home'
   get 'static_pages/about'
+
   resources :transactions
-  root 'accounts#index', as: 'root'
   resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
