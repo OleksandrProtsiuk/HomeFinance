37572a1b0d3b0d4060328e405d926401d20932a8
alex <alex_is_ok@yahoo.com>
2019-03-02 21:17:17 +0200

    migrations edited

--- db/migrate/20190217164650_create_accounts.rb
+++ db/migrate/20190217164650_create_accounts.rb
@@ -3,8 +3,8 @@ class CreateAccounts < ActiveRecord::Migration[5.2]
     create_table :accounts do |t|
       t.string :title
       t.decimal :value
-      t.string :status
-      t.integer :category_id
+      t.integer :currency
+      t.integer :user_id
 
       t.timestamps
     end
