bc90211b0c988b5fbdfacab3e50f0b05bd823818
alex <alex_is_ok@yahoo.com>
2019-03-02 21:22:55 +0200

    field value deleted

--- db/migrate/20190217164650_create_accounts.rb
+++ db/migrate/20190217164650_create_accounts.rb
@@ -2,7 +2,6 @@ class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
       t.string :title
-      t.decimal :value
       t.integer :currency
       t.integer :user_id
 
