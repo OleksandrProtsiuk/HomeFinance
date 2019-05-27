a0b35da0205b096b0de28c5c83dce08a6d37fc91
alex <alex_is_ok@yahoo.com>
2019-05-28 00:00:08 +0300

    commit

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,6 +1,96 @@
 import Vue from "vue/dist/vue.esm.js";
 import axios from "axios";
 
+// --------------------- Components
+
+Vue.component('donutChart', {
+    template: '#donutTemplate',
+    props: ["initialValues"],
+    data() {
+        return {
+            angleOffset: -90,
+            chartData: [],
+            colors: ['coral', 'lightgreen'],
+            cx: 80,
+            cy: 80,
+            radius: 60,
+            sortedValues: [],
+            strokeWidth: 40,
+        }
+    },
+    computed: {
+        // adjust the circumference to add small white gaps
+        adjustedCircumference() {
+            return this.circumference - 2
+        },
+        circumference() {
+            return 2 * Math.PI * this.radius
+        },
+        dataTotal() {
+            return this.sortedValues.reduce((acc, val) => acc + val)
+        },
+        calculateChartData() {
+            this.sortedValues.forEach((dataVal, index) => {
+                const { x, y } = this.calculateTextCoords(dataVal, this.angleOffset);
+                // start at -90deg so that the largest segment is perpendicular to top
+                const data = {
+                    degrees: this.angleOffset,
+                    textX: x,
+                    textY: y
+                };
+                this.chartData.push(data);
+                this.angleOffset = this.dataPercentage(dataVal) * 360 + this.angleOffset
+            })
+        },
+        sortInitialValues() {
+            return this.sortedValues = this.initialValues.sort((a,b) => b-a)
+        }
+    },
+    methods: {
+        calculateStrokeDashOffset(dataVal, circumference) {
+            const strokeDiff = this.dataPercentage(dataVal) * circumference;
+            return circumference - strokeDiff
+        },
+        calculateTextCoords(dataVal, angleOffset) {
+            // t must be radians
+            // x(t) = r cos(t) + j
+            // y(t) = r sin(t) + j
+
+            const angle = (this.dataPercentage(dataVal) * 360) / 2 + angleOffset;
+            const radians = this.degreesToRadians(angle);
+
+            const textCoords = {
+                x: this.radius * Math.cos(radians) + this.cx,
+                y: this.radius * Math.sin(radians) + this.cy
+            };
+            return textCoords
+        },
+        degreesToRadians(angle) {
+            return angle * (Math.PI / 180)
+        },
+        dataPercentage(dataVal) {
+            return dataVal / this.dataTotal
+        },
+        percentageString(dataVal) {
+            return `${Math.round(this.dataPercentage(dataVal) * 100)}%`
+        },
+        returnCircleTransformValue(index) {
+            return `rotate(${this.chartData[index].degrees}, ${this.cx}, ${this.cy})`
+        },
+        segmentBigEnough(dataVal) {
+            return Math.round(this.dataPercentage(dataVal) * 100) > 5
+        }
+    },
+    mounted() {
+        this.sortInitialValues
+        this.calculateChartData
+    }
+});
+
+
+
+// ------------------------ Elements
+
 new Vue({
     el: '#income-transaction',
     data() {
@@ -309,5 +399,39 @@ new Vue({
             });
             return value;
         }
+    }
+});
+
+new Vue({
+    el: "#totales",
+    data: {
+        values: [0, 0],
+        transactions: null
+    },
+    methods: {
+        totals () {
+            let income = 0.0;
+            let expanse = 0.0;
+
+            if (this.transactions === null) {
+                axios.get( '/transactions.json').then(response => {
+                    this.transactions = response.data
+                });
+            }
+
+            this.transactions.forEach(function (element) {
+                if (parseFloat(element.amount) >= 0 ) {
+                    income += parseFloat(element.amount)
+                }
+                else {
+                    expanse += parseFloat(element.amount)
+                }
+            });
+
+            return this.values = [income, expanse];
+        }
+    },
+    beforeMount() {
+        this.totals()
     }
 });
